adbe3b80ce9381d1ee3728757cc2d3805fffc14c
Ziheng JING <eliottjing@gmail.com>
2023-05-18 21:05:53 +0930

    add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
+import logo from "./logo.svg";
+import { GlobalOutlined } from "@ant-design/icons";
 
 function App() {
+  const menu = (
+    <Menu>
+      <Menu.Item>Chinese</Menu.Item>
+      <Menu.Item>English</Menu.Item>
+    </Menu>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles["app-header"]}>
+        {/* top-header */}
+        <div className={styles["top-header"]}>
+          <div className={styles.inner}>
+            <Typography.Text>Make Your Travel Happy</Typography.Text>
+            <Dropdown.Button
+              style={{ marginLeft: 15 }}
+              overlay={menu}
+              icon={<GlobalOutlined />}
+              className={styles["inline-dropdown-button"]}
+            >
+              Language
+            </Dropdown.Button>
+            <Button.Group className={styles["button-group"]}>
+              <Button>Signup</Button>
+              <Button>Signin</Button>
+            </Button.Group>
+          </div>
+        </div>
+        <Layout.Header className={styles["main-header"]}>
+          <img src={logo} alt="logo" className={styles["App-logo"]} />
+          <Typography.Title level={3} className={styles.title}>
+            React Travel
+          </Typography.Title>
+          <Input.Search
+            placeholder="Please input your destination!"
+            className={styles["search-input"]}
+          />
+        </Layout.Header>
+        <Menu
+          mode={"horizontal"}
+          className={styles["main-menu"]}
+          items={[
+            { key: 1, label: "Home" },
+            { key: 2, label: "Group" },
+            { key: 3, label: "Personal" },
+            { key: 4, label: "Hotal" },
+            { key: 5, label: "Visa" },
+            { key: 6, label: "Travel Study" },
+            { key: 7, label: "Business" },
+            { key: 8, label: "VIP" },
+            { key: 9, label: "Insurance" },
+            { key: 10, label: "Local" },
+          ]}
+        />
+      </div>
+      <Layout.Footer>
+        <Typography.Title level={3} style={{ textAlign: "center" }}>
+          Copyright @ React Travel
+        </Typography.Title>
+      </Layout.Footer>
     </div>
   );
 }
